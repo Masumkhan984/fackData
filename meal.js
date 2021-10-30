95c7e7f14e90b8f010c3c2ee1faf597abae5ed1d
Masumkhan984 <masumkhan9984@gmail.com>
2021-10-30 15:53:23 +0600

    first commit

---
+++ meal.js
@@ -0,0 +1,25 @@
+// const meals =[
+//     {
+//         breakfast=[
+//             {
+//                 mealId:1,
+//                 img:"https://i.ibb.co/KxwTqLb/breakfast1.png",
+//                 name: "Fried Omlate and Pork",
+//             },
+//             {
+//                 mealId:2,
+//                 img:"https://i.ibb.co/dpJsJ2Q/breakfast2.png",
+//                  name: "Healthy Fruits"
+//              },
+//           ]
+//     },
+//     {
+//         lunch=[
+//             {
+//                 mealId:2,
+//                 img:"https://i.ibb.co/p49Jwkj/breakfast3.png",
+//                 name:"healthy lunch"
+//             }
+//         ]
+//     }
+// ]
